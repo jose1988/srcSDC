50539278be320ee01b8b80fd2c708cc624ce775a
Jose fuentes <jose.fuentes@pangeatech.com.ve>
2014-02-11 14:32:38 -0430

    usuario

--- java/com/seguroshorizonte/sistemadecorrespondecia/sessionfacade/UsuarioFacade.java
+++ java/com/seguroshorizonte/sistemadecorrespondecia/sessionfacade/UsuarioFacade.java
@@ -5,9 +5,11 @@
 package com.seguroshorizonte.sistemadecorrespondecia.sessionfacade;
 
 import com.seguroshorizonte.sistemadecorrespondecia.entidades.Usuario;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -15,6 +17,7 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class UsuarioFacade extends AbstractFacade<Usuario> {
+
     @PersistenceContext(unitName = "SistemaDeCorrespondeciaPU")
     private EntityManager em;
 
@@ -26,5 +29,76 @@ public class UsuarioFacade extends AbstractFacade<Usuario> {
     public UsuarioFacade() {
         super(Usuario.class);
     }
-    
+
+    public Usuario consultarUsuarioXUser(String user) {
+        Usuario Registro = new Usuario();
+        try {
+            Registro = (Usuario) (em.createNamedQuery("Usuario.findByUserusu").setParameter("userusu", user).getSingleResult());
+        } catch (Exception e) {
+
+
+            return Registro;
+        }
+
+
+        return Registro;
+    }
+
+    public List<Usuario> listarUsuarios(String status) {
+        List<Usuario> lista;
+        lista = em.createNamedQuery("Usuario.findByStatususu").setParameter("statususu", status).getResultList();
+        return lista;
+    }
+
+    public List<Usuario> listar() {
+        List<Usuario> c = null;
+
+        c = (List<Usuario>) em.createNamedQuery("Usuario.findAll").getResultList();
+        return c;
+    }
+
+    public void insertar(Usuario registro) {
+
+        this.create(registro);
+
+    }
+
+    public void editar(Usuario registro) {
+
+        this.edit(registro);
+
+    }
+
+    public void deshabilitar(String ID) {
+        Query q = em.createNativeQuery("UPDATE Usuario SET statususu='0' WHERE id=?");
+        q.setParameter(1, ID);
+        q.executeUpdate();
+    }
+
+    public void habilitar(String ID) {
+        Query q = em.createNativeQuery("UPDATE Usuario SET statususu='1' WHERE id=?");
+        q.setParameter(1, ID);
+        q.executeUpdate();
+    }
+
+    public Usuario consultarUsuario(String idUsuario) {
+        Usuario Registro;
+        Registro = this.find(idUsuario);
+        return Registro;
+    }
+
+    public void editarUsuario(Usuario Registro) {
+        Query q = em.createNativeQuery("UPDATE USUARIO "
+                + "SET APELLIDOUSU=?,CORREOUSU=?,DIRECCIONUSU=?,DIRECCION2USU=?,NOMBREUSU=?,TELEFONOUSU=?,TELEFONO2USU=?"
+                + "WHERE IDUSU=?");
+        q.setParameter(1, Registro.getApellidousu());
+        q.setParameter(2, Registro.getCorreousu());
+        q.setParameter(3, Registro.getDireccion2usu());
+        q.setParameter(4, Registro.getDireccionusu());
+        q.setParameter(5, Registro.getNombreusu());
+        q.setParameter(6, Registro.getTelefono2usu());
+        q.setParameter(7, Registro.getTelefonousu());
+        q.setParameter(8, Registro.getIdusu());
+        q.executeUpdate();
+    }
 }
