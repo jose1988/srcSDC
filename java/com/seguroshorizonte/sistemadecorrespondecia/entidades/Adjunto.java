e8e45a2f1ebe1436afb33db6c91c3588a0c52052
Mariela Arboleda <luz.arboleda25@gmail.com>
2014-03-07 15:08:35 -0430

    secuencia

--- java/com/seguroshorizonte/sistemadecorrespondecia/entidades/Adjunto.java
+++ java/com/seguroshorizonte/sistemadecorrespondecia/entidades/Adjunto.java
@@ -9,11 +9,14 @@ import java.math.BigDecimal;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -36,6 +39,8 @@ public class Adjunto implements Serializable {
     private static final long serialVersionUID = 1L;
     // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ADJUNTOSEQ")
+    @SequenceGenerator(name = "ADJUNTOSEQ", sequenceName = "SEQ_ADJUNTO", allocationSize = 1)
     @Basic(optional = false)
     @NotNull
     @Column(name = "IDADJ")
