bba183ede2c377225b566875e33fda50bf67c37f
Jose fuentes <jose.fuentes@pangeatech.com.ve>
2014-05-05 09:52:55 -0430

    proveedor secuencia

--- java/com/seguroshorizonte/sistemadecorrespondecia/entidades/Proveedorsede.java
+++ java/com/seguroshorizonte/sistemadecorrespondecia/entidades/Proveedorsede.java
@@ -9,11 +9,14 @@ import java.math.BigDecimal;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -33,9 +36,10 @@ public class Proveedorsede implements Serializable {
 
     private static final long serialVersionUID = 1L;
     // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PROVEEDORSEDE")
+    @SequenceGenerator(name = "SEQ_PROVEEDORSEDE", sequenceName = "SEQ_PROVEEDORSEDE", allocationSize = 1)
     @Id
     @Basic(optional = false)
-    @NotNull
     @Column(name = "IDPSE")
     private BigDecimal idpse;
     @JoinColumn(name = "IDSED", referencedColumnName = "IDSED")
